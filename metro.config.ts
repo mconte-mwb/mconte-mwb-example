327245d3f3ae9ac4d2dd3494e798df39cc856591
mconte-mwb <mconte@malwarebytes.com>
2024-01-21 15:01:30 +0100

    fix: storybook setup

--- metro.config.ts
+++ metro.config.ts
@@ -2,4 +2,8 @@ import { getDefaultConfig } from 'expo/metro-config';
 
 const config = getDefaultConfig(__dirname);
 
+// TODO: by next link this seems to be required but I forgot to add and the project was starting anyway locally on ExpoGo
+// @ts-expect-error this is the explained method in https://github.com/storybookjs/react-native/blob/next/MANUAL_SETUP.md
+config.resolver.resolverMainFields.unshift('sbmodern');
+
 module.exports = config;
