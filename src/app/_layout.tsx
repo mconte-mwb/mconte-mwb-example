086f49994528a84258847c02a307e7c3741384af
mconte-mwb <mconte@malwarebytes.com>
2024-01-21 10:54:29 +0100

    style: load Roboto font

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,6 +6,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
 import { useColorScheme } from '@/components/useColorScheme';
+import { useRobotoFonts } from '@/hooks';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -21,23 +22,15 @@ export const unstable_settings = {
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+  const {isLoading: isLoadingFont} = useRobotoFonts();
 
   useEffect(() => {
-    if (loaded) {
+    if (!isLoadingFont) {
       SplashScreen.hideAsync();
     }
-  }, [loaded]);
+  }, [isLoadingFont]);
 
-  if (!loaded) {
+  if (isLoadingFont) {
     return null;
   }
 
